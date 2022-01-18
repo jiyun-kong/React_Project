e5dc981b909ca06dcd59a97528a121569195003e
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-18 23:25:55 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import styles from './App.module.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+
+    </div>
+
+
+  );
+}
+
+export default App;
