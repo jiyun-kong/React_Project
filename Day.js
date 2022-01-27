7fb7b6df69e3621e3021f1e9d21fea7d45d91568
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-27 20:33:17 +0900

    Update Day.js

--- Day.js
+++ Day.js
@@ -1,17 +1,26 @@
-import dummy from "../db/data.json"
 import Word from "./Word";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Day() {
     const urlDay = Number(useParams().day);
-    const wordList = dummy.words.filter(ele => ele.day === urlDay);
+    //const wordList = dummy.words.filter(ele => ele.day === urlDay);
+    const [words, setWords] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${urlDay}`).then(response => {
+            return response.json()
+        }).then(data => {
+            setWords(data);
+        });
+    }, []);
 
     return (
         <>
             <h2>Day {urlDay}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
+                    {words.map(word => (
                         <Word word={word} key={word.id} />
                     ))}
                 </tbody>
